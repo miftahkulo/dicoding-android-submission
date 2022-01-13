06b9c98a7cadb99741058938e8888641cb444a2f
Indra Mahkota <indramahkota1@gmail.com>
2022-01-13 09:26:57 +0700

    merge movie and tv fragment

--- app/src/main/java/com/indramahkota/app/presentation/movie/MovieListFragment.kt
+++ app/src/main/java/com/indramahkota/app/presentation/movie/MovieListFragment.kt
@@ -5,25 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
-import com.indramahkota.app.databinding.FragmentMovieBinding
+import androidx.navigation.fragment.navArgs
+import com.indramahkota.app.databinding.FragmentListMovieBinding
 import com.indramahkota.app.presentation.adapter.MovieAdapter
 import com.indramahkota.app.viewmodel.MovieViewModel
 import com.indramahkota.common.base.BaseBindingFragment
 import com.indramahkota.common.base.BaseModel
 import com.indramahkota.common.base.LoadingModel
+import com.indramahkota.domain.model.Movie
 import com.indramahkota.domain.utils.Resource
 import timber.log.Timber
 
-class MovieFragment : BaseBindingFragment() {
-    private var _binding: FragmentMovieBinding? = null
+class MovieListFragment : BaseBindingFragment() {
+    private var _binding: FragmentListMovieBinding? = null
     private val binding get() = _binding!!
+
+    private val args: MovieListFragmentArgs by navArgs()
 
     private val movieViewModel: MovieViewModel by activityViewModels()
 
     private lateinit var movieAdapter: MovieAdapter
 
     override fun bindFragment(inflater: LayoutInflater, container: ViewGroup?): View {
-        _binding = FragmentMovieBinding.inflate(inflater, container, false)
+        _binding = FragmentListMovieBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -43,21 +47,31 @@ class MovieFragment : BaseBindingFragment() {
     }
 
     private fun observeViewModel() {
-        movieViewModel.getMovies.observe(this) {
-            when (it) {
-                is Resource.Loading -> {
-                    Timber.d("Loading")
-                    for (i in 1..10) {
-                        movieAdapter.addItem(LoadingModel())
-                    }
+        if (!args.isTv) {
+            movieViewModel.getMovies.observe(this) {
+                handleResource(it)
+            }
+        } else {
+            movieViewModel.getTv.observe(this) {
+                handleResource(it)
+            }
+        }
+    }
+
+    private fun handleResource(resource: Resource<List<Movie>>) {
+        when (resource) {
+            is Resource.Loading -> {
+                Timber.d("Loading")
+                for (i in 1..10) {
+                    movieAdapter.addItem(LoadingModel())
                 }
-                is Resource.Success -> {
-                    Timber.d("Success")
-                    movieAdapter.setDatas(it.data as List<BaseModel>)
-                }
-                is Resource.Error -> {
-                    Timber.d("Error")
-                }
+            }
+            is Resource.Success -> {
+                Timber.d("Success")
+                movieAdapter.setDatas(resource.data as List<BaseModel>)
+            }
+            is Resource.Error -> {
+                Timber.d("Error")
             }
         }
     }
